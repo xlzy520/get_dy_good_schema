61769856a060d9851a01b280b14cef79898dcc3d
xlzy <1017866168@qq.com>
2024-04-13 16:27:24 +0800

    u

--- app.js
+++ app.js
@@ -53,14 +53,6 @@ app.get('/upload', (req, res) => {
   program.stdin.write(imagePath + '\n');
 });
 
-
-// const getLicense = (key) => {
-//   return axios.get('https://wj.xlzy520.cn/license/bind', {
-//
-//   }
-//
-// }
-
 const LicenseMap = {
   'bqqysv28vkh': {
     name: '鱼跃传媒',
@@ -84,23 +76,25 @@ const LicenseMap = {
 
 
 // 实现一个Map，要求每个key，每天最多只能上传300次，超过300次则返回错误信息。每天0点清空Map
-const uploadMap = new Map();
+const GlobalData = {
+  uploadMap: {},
+}
 
 setInterval(() => {
   // 每天0点30分清空Map
   const currentHour = dayjs().format('HH');
   const currentMinute = dayjs().format('mm');
   if (currentHour === '00' && currentMinute <30){
-    uploadMap.clear();
+    GlobalData.uploadMap = {}
   }
 }, 10 * 60 * 1000);
 
 app.get('/clear', (req, res) => {
   const { key } = req.query;
   if (!key) {
-    uploadMap.clear();
+    GlobalData.uploadMap = {}
   } else {
-    uploadMap.delete(key);
+    GlobalData.uploadMap[key] = 0;
   }
   res.send('清空成功');
 })
@@ -126,28 +120,30 @@ app.post('/upload', upload.single('image'), (req, res) => {
     return;
   }
   
-  if (!uploadMap.has(key)) {
-    uploadMap.set(key, 1);
+  console.log(GlobalData, '===========打印的 ------ ');
+  
+  if (GlobalData.uploadMap[key] === undefined) {
+    GlobalData.uploadMap[key] = 0;
   } else {
-    uploadMap.set(key, uploadMap.get(key) + 1);
+    GlobalData.uploadMap[key] += 1;
   }
   
- 
-  
-  if (uploadMap.get(key) > 4) {
-    res.status(500).send('上传次数超过限制，请联系gpt');
+  if (GlobalData.uploadMap[key] > 200) {
+    res.status(500).send({
+      message: '超过每天上传次数，请联系gpt',
+    });
     return;
   }
-  // res.send({
-  //   schema: 'sslocal://polaris/proxy?enter_from=qrcode&invite_from=qrcode&invite_token=sta1-NNQgxwSMPVOSHhcaiz0JWJTauuKceasal3JB3OdH8c_6SnawW-nZQ930_jDJaaOQvDlNZBbUlTyBGKoitlTPpR8cZe453N22IJwQ1CmrqmJtr6L7B5rEuHFaQAdI_8n-Uu4BEm44kMKLb2jefseVVQ&polaris_share_timestamp=onecent_bargain_invite_2329_1712934403125753826&scene_key=main&ug_activity_id=onecent_bargain&allow_pending_ms=5000',
-  //   now: dayjs().format('YYYY-MM-DD HH:mm:ss')
-  // });
-  //
-  // return;
+  res.send({
+    schema: 'sslocal://polaris/proxy?enter_from=qrcode&invite_from=qrcode&invite_token=sta1-NNQgxwSMPVOSHhcaiz0JWJTauuKceasal3JB3OdH8c_6SnawW-nZQ930_jDJaaOQvDlNZBbUlTyBGKoitlTPpR8cZe453N22IJwQ1CmrqmJtr6L7B5rEuHFaQAdI_8n-Uu4BEm44kMKLb2jefseVVQ&polaris_share_timestamp=onecent_bargain_invite_2329_1712934403125753826&scene_key=main&ug_activity_id=onecent_bargain&allow_pending_ms=5000',
+    now: dayjs().format('YYYY-MM-DD HH:mm:ss')
+  });
+
+  return;
   
   const program = spawn('test.exe');
   const imagePath = req.file.path; // 获取上传文件的路径
-  
+
   program.stdout.on('data', (data) => {
     console.log(`${currentTime} 输出: ${data}`);
     if (data.toString().includes('sta')) {
@@ -160,12 +156,12 @@ app.post('/upload', upload.single('image'), (req, res) => {
       program.kill();
     }
   });
-  
+
   program.stderr.on('data', (data) => {
     console.error(`错误: ${data}`);
     res.status(500).send(data); // 发生错误时，将错误信息发送给客户端
   });
-  
+
   program.stdin.write(imagePath + '\n');
 });
 
